ad29a6ad0436c369414bb18532d0c8d6c7a18a5f
saman <mafhh.end@gmail.com>
2022-04-03 18:39:10 +0430

    init

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.quizapp
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        if(Build.VERSION.SDK_INT >=30){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+
+    }
+}
